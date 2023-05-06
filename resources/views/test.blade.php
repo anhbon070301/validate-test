a429527c37d4d09c5df38a69966ad2a06b6fa64a
you <anhemtn114@gmail.com>
2023-05-06 20:09:36 +0700

    update

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,16 +1,40 @@
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.min.js"></script>
 <form id="myform" action="" method="post">
     <input type="text" name="email" id="email">
     <input type="text" name="pass" id="pass">
+    <div class="form-group">
+        <div class="file-loading">
+            <label>Hình đại diện</label>
+            <input id="sp_hinh" type="file" name="sp_hinh">
+            <div class="preview-upload">
+                <img id='sp_hinh-upload' />
+            </div>
+        </div>
+    </div>
     <input type="submit" value="Submit">
 </form>
 <script>
     $(document).ready(function() {
+        // Sử dụng FileReader để đọc dữ liệu tạm trước khi upload lên Server
+        function readURL(input) {
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+                reader.onload = function(e) {
+                    $('#sp_hinh-upload').attr('src', e.target.result);
+                }
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
+
+        // Bắt sự kiện, ngay khi thay đổi file thì đọc lại nội dung và hiển thị lại hình ảnh mới trên khung preview-upload
+        $("#sp_hinh").change(function() {
+            readURL(this);
+        });
         $("#myform").validate({
             rules: {
                 pass: {
-                    required:true
+                    required: true
                 },
                 email: {
                     required: true,
